4eca1336d29d2484cc5d4fa807a862779cc6cd7e
Daniel Mundra <dmundra@users.noreply.github.com>
2024-09-21 14:44:00 -0700

    Updated dependencies and updated github workflows. (#30)
    * Updated dependencies and updated github workflows.
    
    * Pinning svelte version to 3.23.0 and removing cypress publishing test results.
    
    * Removed trash can icon as it was not displaying.

--- src/components/Related.svelte
+++ src/components/Related.svelte
@@ -15,14 +15,7 @@
 
 <style>
   .Related__Control--delete {
-    font-size: 1em;;
     float: right;
-    margin-left: 1em;
-    margin-top: .25em;
-    padding: 0;
-    border: 0;
-    background-color: transparent;
-    color: currentColor;
   }
   .Related {
     background: var(--trans-line-grey);
@@ -54,25 +47,7 @@
     type="button"
     class="Related__Control--delete"
     on:click="{dispatchDelete}"
-    >
-      <svg
-        focusable="false"
-        aria-hidden="true"
-        role="presentation"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round">
-          <polyline points="3 6 5 6 21 6"/>
-          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-          <line x1="10" y1="11" x2="10" y2="17" stroke="#ffffff"/>
-          <line x1="14" y1="11" x2="14" y2="17" stroke="#ffffff" />
-      </svg>
-      <span class="visuallyhidden">Delete Related OpenACR</span>
-    </button>
+    >Delete</button>
     <fieldset>
       <legend>Related OpenACR {count}</legend>
       <div class="related-input">
